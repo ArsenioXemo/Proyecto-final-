f45a995d6f28f5cccef8b02d9da0d1298e5c7237
angelica.solis.avila.contractor <angelica.solis.avila.contractor@bbva.com>
2022-04-07 18:27:00 -0500

    Eliminar merge

--- Proyecto_Noe/force-app/main/default/aura/componenteAura_noevasher/componenteAura_noevasherController.js
+++
@@ -1,9 +0,0 @@
-({
-    doInit : function(component, event, helper) {
-        helper.doInit(component, event);
-    },
-
-    save: function(component, event, helper){
-        helper.save(component, event);
-    }
-})
