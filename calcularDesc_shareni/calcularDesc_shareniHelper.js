1930e378ec6bbd461125a332a5586d5409d8f9de
Shareni <svargas@indracompany.com>
2022-04-18 16:15:12 -0500

    actualizado svp

--- calcularDesc_shareni/calcularDesc_shareniHelper.js
+++ calcularDesc_shareni/calcularDesc_shareniHelper.js
@@ -2,8 +2,15 @@
     helperMethod : function(component, event) {
         var action = component.get("c.calcula");
         var recordID = component.get("v.recordId");
+        var recAccID = component.get("v.OpportunityObject.AccountId");
+        
+        //alert("ID oportunidad: "+recordID);
+        //alert("ID cuenta: "+recAccID);
 
-        action.setParam({Idpp: recordID});
+        action.setParams({'Idpp': recordID, 'Idcuenta': recAccID});
+        action.setCallback(this, function(Response){
+            alert("status: "+Response.getState());
+        })
         $A.enqueueAction(action);
     }
 })
